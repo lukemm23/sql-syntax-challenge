b5aade1a3c110818918e096f4d313f797cf145f8
lukemm23 <lukemm23@gmail.com>
2019-11-25 14:22:54 -0600

    question 5,6 answered

--- database.sql
+++ database.sql
@@ -17,15 +17,20 @@ WHERE "transactions_attempted" = 0 AND "account_balance" = 0;
 
 
 -- 4. How do you select all users that have attempted 9 or more transactions?
+SELECT * FROM "accounts"
+WHERE "transactions_attempted" >= 9;
 
 
 -- 5. How do you get the username and account balance of the 3 users with the highest balances, 
 -- sorted highest to lowest balance? NOTE: Research LIMIT
+SELECT "username", "account_balance" FROM "accounts" 
+ORDER BY "account_balance" DESC LIMIT 3 ; 
 
 
 -- 6. How do you get the username and account balance of the 3 users with the lowest balances, 
 -- sorted lowest to highest balance?
-
+SELECT "username", "account_balance" FROM "accounts"
+ORDER BY "account_balance" ASC LIMIT 3 ; 
 
 -- 7. How do you get all users with account balances that are more than $100?
 
